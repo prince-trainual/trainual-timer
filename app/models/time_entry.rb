c6a5db0b84b4ff9b15444001825c202d409345ab
Prince Olorunyomi <prince@trainual.com>
2021-04-16 14:15:25 -0500

    TimeEntry: add validations

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,3 +1,13 @@
 class TimeEntry < ApplicationRecord
   belongs_to :user
+
+  validates :starts_at, presence: true
+  validates :finishes_at, presence: true, if: -> { self.persisted? }
+  validate :only_one_running 
+
+  private
+
+  def only_one_running
+    errors.add(:running, 'already running') if user&.timer_running?
+  end
 end
