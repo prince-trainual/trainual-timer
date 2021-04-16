8d4db534535e0ae79df05813adf76a8d74385fae
Prince Olorunyomi <prince@trainual.com>
2021-04-16 13:09:40 -0500

    Add TimeEntriesController

---
+++ app/controllers/time_entries_controller.rb
@@ -0,0 +1,52 @@
+class TimeEntriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_time_entry, only: %i[edit update]
+
+  # GET /time_entries or /time_entries.json
+  def index
+    @time_entries = currenct_user.time_entries
+  end
+
+  # GET /time_entries/1/edit
+  def edit
+  end
+
+  # POST /time_entries or /time_entries.json
+  def create
+    @time_entry = currenct_user.time_entries.new(time_entry_params)
+
+    respond_to do |format|
+      if @time_entry.save
+        format.html { redirect_to @time_entry, notice: "Time entry was successfully created." }
+        format.json { render :show, status: :created, location: @time_entry }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @time_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /time_entries/1 or /time_entries/1.json
+  def update
+    respond_to do |format|
+      if @time_entry.update(time_entry_params)
+        format.html { redirect_to @time_entry, notice: "Time entry was successfully updated." }
+        format.json { render :show, status: :ok, location: @time_entry }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @time_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_time_entry
+      @time_entry = currenct_user.time_entries.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def time_entry_params
+      params.require(:time_entry).permit(:starts_at, :finishes_at)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'time_entries#index'
+
+  resource :time_entries, only: %i[index create edit update]
 end
