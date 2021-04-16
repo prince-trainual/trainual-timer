713b21817aba16b38ecabb182f7c35e6480b5800
Prince Olorunyomi <prince@trainual.com>
2021-04-16 14:14:56 -0500

    Add User#timer_running?

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :time_entries, dependent: :destroy
+
+  def timer_running?
+    TimeEntry.exists?(user_id: self.id, finishes_at: nil)
+  end  
 end
